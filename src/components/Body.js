33f5f166d02521ab8460bfc7700b73de575c5de0
Mona Lisa <vinaybasargekar@gmail.com>
2024-06-26 12:15:57 +0530

    feat: added CORS addon in API

--- src/components/Body.js
+++ src/components/Body.js
@@ -14,7 +14,7 @@ const Body = () => {
 
 	const fetchData = async () => {
 		const response = await fetch(
-			"https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+			"https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 		);
 		const data = await response.json();
 
