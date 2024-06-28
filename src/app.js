50a9dda848fef3225a387a09ff16778f36242530
Mona Lisa <vinaybasargekar@gmail.com>
2024-06-28 20:52:59 +0530

    feat: routing between pages & improved UI

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import { createBrowserRouter,RouterProvider,Outlet } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Body from "./components/Body";
 import About from "./components/about";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+
 
 const FoodApp = () => {
 	return (
 		<div className="foodApp">
 			<Navbar />
-			<Body />
+			<Outlet/>
 		</div>
 	);
 };
@@ -19,13 +22,24 @@ const BrowserRouter = createBrowserRouter([
 	{
 		path: "/",
 		element: <FoodApp />,
+		errorElement: <Error />,
+		children: [
+			{
+				path: "/",
+				element: <Body />,
+			},
+			{
+				path: "/about",
+				element: <About />,
+			},
+			{
+				path: "/contact",
+				element: <Contact />,
+			},
+		],
 	},
-	{
-		path: "/about",
-		element: <About />,
-	}
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<FoodApp />);
+root.render(<RouterProvider router={BrowserRouter}/>);
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
-
-import { APP_LOGO } from "../utils/constants";
+import { Link } from "react-router-dom";
+import { APP_LOGO, SVG_CART } from "../utils/constants";
 
 const Navbar = () => {
 	return (
@@ -7,11 +7,18 @@ const Navbar = () => {
 			<img src={APP_LOGO} alt="app-logo" className="app-logo" />
 			<div className="nav-right">
 				<div className="nav-tabs">
-					<ul>
-						<li>Home</li>
-						<li>Contact Us</li>
-						<li>About</li>
+					<ul className="nav-list">
+						<Link to={"/"} style={{ textDecoration: "none" }}>
+							<li className="nav-item">Home</li>
+						</Link>
+						<Link to={"/contact"} style={{ textDecoration: "none" }}>
+							<li className="nav-item">Contact us</li>
+						</Link>
+						<Link to={"/about"} style={{ textDecoration: "none" }}>
+							<li className="nav-item">About</li>
+						</Link>
 					</ul>
+
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
@@ -26,13 +33,14 @@ const Navbar = () => {
 							d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
 						/>
 					</svg>
+
 					<svg
 						viewBox="0 0 36 36"
 						fill="none"
 						role="img"
 						xmlns="http://www.w3.org/2000/svg"
-						width="80"
-						height="80"
+						width="36"
+						height="36"
 						className="userLogo"
 					>
 						<mask
