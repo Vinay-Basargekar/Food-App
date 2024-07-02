00ef927b6137076a3de1bbcfe4d62514f5f5b99d
Mona Lisa <vinaybasargekar@gmail.com>
2024-07-02 18:34:45 +0530

    init

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import Shimmer from "./shimmer";
 import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
