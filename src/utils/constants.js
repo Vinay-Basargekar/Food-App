6fae111b68ac7a3478d1b73f7fdf59afa1723803
Guffy-san <vinaybasargekar13@gmail.com>
2024-07-05 15:41:11 +0530

    init

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,4 +5,4 @@ export const FOOD_IMAGES =
 	"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const MENU_API =
-	"https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5204303&lng=73.8567437&restaurantId=";
+	"https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.528913&lng=73.87441989999999&restaurantId=";
